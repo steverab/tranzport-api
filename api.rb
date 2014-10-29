c48d241469e6b2fc4313b24e5ceb945f62ebd3f1
Stephan Rabanser <steverab93@gmail.com>
2014-10-29 22:45:07 +0100

    Add experimental departures endpoint

--- api.rb
+++ api.rb
@@ -1,9 +1,29 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+require './extensions/str.rb'
 
 before do
-  content_type 'text/plain'
+    content_type 'application/json'
 end
 
 get '/' do
-  "Welcome to the Tranzit API!"
+    content_type 'text/plain'
+    "Welcome to the Tranzit API!"
+end
+
+get '/departures/:stationName' do
+    page = Nokogiri::HTML(open("http://www.mvg-live.de/ims/dfiStaticAnzeige.svc?haltestelle=#{params[:stationName]}&ubahn=checked&bus=checked&tram=checked&sbahn=checked"))
+    resp = []
+    page.search('//span').remove
+    page.xpath('//tr[starts-with(@class,"row")]').each do |departure|
+        line = departure.css('td.lineColumn')[0].text
+        destination = departure.css('td.stationColumn')[0].text
+        destination = destination.strip_control_characters
+        minutes = departure.css('td.inMinColumn')[0].text
+        resp << {:line => "#{line}", :destination => "#{destination}", :minutes => "#{minutes}"}
+    end
+    resp.to_json
 end
